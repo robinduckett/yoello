87e6dd59e8eadf6dbb481504a6d41ec130bdb425
Robin Duckett <robin.duckett@gmail.com>
2020-05-02 15:28:53 +0100

    Added second test, updated jest configuration

--- __tests__/components/navigation.spec.tsx
+++ __tests__/components/navigation.spec.tsx
@@ -2,20 +2,38 @@ import * as React from "react";
 import { mount } from "enzyme";
 import Navigation from "../../components/navigation";
 import { GlobalContext } from "../../context/global";
+import { reducer } from "../../context/reducer";
 
 beforeEach(() => {
   jest.resetModules();
 });
 
+function Provider({ children, initialState }) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ state, dispatch }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
+
 describe("Navigation Component", () => {
   it("should render without throwing an error", function () {
     const wrap = mount(
-      <GlobalContext.Provider
-        value={{ state: { category: "pizza" }, dispatch: jest.fn() }}
-      >
+      <Provider initialState={{ category: "pizza" }}>
         <Navigation />
-      </GlobalContext.Provider>
+      </Provider>
     );
     expect(wrap.find("li").first().text()).toBe("Pizza");
+  });
+
+  it("should render with a different current category when the state is set", () => {
+    const wrap = mount(
+      <Provider initialState={{ category: "steak" }}>
+        <Navigation />
+      </Provider>
+    );
+    expect(wrap.find("li.current").text()).toBe("Steak");
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
+  moduleFileExtensions: ["js", "ts", "tsx"],
   testMatch: ["**/*.(test|spec).(ts|tsx)"],
   globals: {
     "ts-jest": {
-      useBabelrc: true,
-      tsConfigFile: "jest.tsconfig.json",
+      tsConfig: "jest.tsconfig.json",
+      babelConfig: true,
     },
   },
   coveragePathIgnorePatterns: ["/node_modules/", "enzyme.js"],
-  setupTestFrameworkScriptFile: "<rootDir>/enzyme.js",
+  setupFilesAfterEnv: ["<rootDir>/enzyme.js"],
   coverageReporters: ["json", "lcov", "text", "text-summary"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/mocks.js",
     "\\.(css|less|scss)$": "<rootDir>/__mocks__/mocks.js",
   },
+  preset: "ts-jest",
 };
